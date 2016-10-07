13718c024006eb0fa7e9b7ca84e9b7fb444338ff
ianyuen <thanhianyuen@gmail.com>
2016-10-07 10:22:44 +0700

    add Device class grant permission CHANGE_NETWORK_STATE

---
+++ app/src/main/java/com/iansoft/android/Device.java
@@ -0,0 +1,33 @@
+package com.iansoft.android;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.net.ConnectivityManager;
+import android.content.Context;
+
+public class Device {
+	private static Device m_sInstance = null;
+
+	public static Device GetInstance() {
+		if (m_sInstance == null) {
+			m_sInstance = new Device();
+		}
+		return m_sInstance;
+	}
+
+	private void setMobileDataEnabled(Context context, boolean enabled) {
+		try {
+			final ConnectivityManager conman = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+			final Class conmanClass = Class.forName(conman.getClass().getName());
+			final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+			iConnectivityManagerField.setAccessible(true);
+			final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+			final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
+			final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+			setMobileDataEnabledMethod.setAccessible(true);
+
+			setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
+		} catch (Exception e) {}
+	}
+}
