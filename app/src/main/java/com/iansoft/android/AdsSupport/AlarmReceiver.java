a4dae3fb589e28a4bfd899f9e84b89b5023dcf33
ianyuen <thanhianyuen@gmail.com>
2016-11-14 14:40:20 +0700

    remove useless code

--- app/src/main/java/com/iansoft/android/AdsSupport/AlarmReceiver.java
+++ app/src/main/java/com/iansoft/android/AdsSupport/AlarmReceiver.java
@@ -10,23 +10,5 @@ public class AlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Log.print();
-
-		int currentSecond = (int)System.currentTimeMillis() / 1000 % 60;
-		Support.GetInstance().TurnOffData();
-		Support.GetInstance().Delay(10 + currentSecond);
-
-		Support.GetInstance().TurnOnData();
-		Support.GetInstance().Delay(20 + currentSecond);
-
-		Support.GetInstance().StartApplication("com.iansoft.android.ExchangeRates");
-		Support.GetInstance().Delay(20 + currentSecond);
-
-		Support.GetInstance().ClickAds();
-		Support.GetInstance().Delay(30 + currentSecond);
-
-		Support.GetInstance().PressHomeKey();
-		Support.GetInstance().Delay(60 + currentSecond);
-
-		Support.GetInstance().StopApplication("com.iansoft.android.ExchangeRates");
 	}
 }
