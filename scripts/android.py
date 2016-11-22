a8f2b1fb4425801a02ba0259ea35b30172f343e7
ianyuen <thanhianyuen@gmail.com>
2016-11-22 13:16:23 +0700

    add Support function

--- scripts/android.py
+++ scripts/android.py
@@ -7,23 +7,29 @@ projectPath = '../app'
 
 gradleVersion = '2.2.0'
 
-def startDDMS():
+def StartDDMS():
 	command = os.environ['ANDROID_TOOLS'] + 'ddms'
 	os.system(command)
+	raw_input('\nPress any key to continue')
+	Support()
 
-def createProject():
+def CreateProject():
 	command = os.environ['ANDROID_TOOLS'] + 'android create project --target android-' + config.TARGET_API + ' --name ' + config.APP_NAME + ' --path ' + projectPath + ' --activity Splash --package ' + config.APP_PACKAGE + ' --gradle --gradle-version ' + gradleVersion
 	os.system(command)
+	raw_input('\nPress any key to continue')
+	Support()
 
-def installAPK():
+def InstallAPK():
 	if config.RELEASE == 0:
 		build = 'debug'
 	else:
 		build = 'release'
 	command = os.environ['PLATFORM_TOOLS'] + 'adb install -r ' + projectPath + '/build/outputs/apk/app-' + build + '.apk'
 	os.system(command)
+	raw_input('\nPress any key to continue')
+	Support()
 
-def makeAPK():
+def MakeAPK():
 	if config.RELEASE == 0:
 		build = 'debug'
 	else:
@@ -32,24 +38,31 @@ def makeAPK():
 	os.chdir(os.getcwd() + '/' + projectPath)
 	command = os.environ['GRADLEW'] + ' assemble' + build
 	os.system(command)
+	raw_input('\nPress any key to continue')
+	Support()
 
-if len(sys.argv) == 1:
-	print 'copy config.py from template folder to root trunk'
-	print 'edit OS environment in setEnv.py'
-	print 'edit config of project in config.py'
-	print ''
-	print 'call android.py create to create project'
-	print 'call android.py make to make apk'
-	print 'call android.py ddms to start DDMS'
-	print 'call android.py install to install apk'
+def Support():
+	print '1 - make apk'
+	print '2 - install apk'
+	print '3 - start DDMS'
+	print '4 - create project'
+	print '5 - exit'
 
-else:
-	argument = str(sys.argv[1])
-	if argument == 'ddms':
-		startDDMS()
-	elif argument == 'make':
-		makeAPK()
-	elif argument == 'create':
-		createProject()
-	elif argument == 'install':
-		installAPK()
+	value = raw_input('Enter your choose: ')
+	if value == '1':
+		MakeAPK()
+	elif value == '2':
+		InstallAPK()
+	elif value == '3':
+		StartDDMS()
+	elif value == '4':
+		CreateProject()
+	elif value == '5':
+		print 'bye bye'
+	else:
+		Support()
+
+def __main__():
+	Support()
+
+__main__()
